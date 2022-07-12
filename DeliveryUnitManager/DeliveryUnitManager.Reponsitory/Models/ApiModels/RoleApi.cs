b7ab961464cb712605e055f5a69bff091027c8ff
ThienPA <gkoster9x@gmail.com>
2022-07-12 14:09:40 +0700

    add RoleApi

--- DeliveryUnitManager/DeliveryUnitManager.Reponsitory/Models/ApiModels/RoleApi.cs
+++ DeliveryUnitManager/DeliveryUnitManager.Reponsitory/Models/ApiModels/RoleApi.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DeliveryUnitManager.Reponsitory.Models.Users;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,16 @@ using System.Threading.Tasks;
 
 namespace DeliveryUnitManager.Reponsitory.Models.ApiModels
 {
-    public class RoleApis
+    public class RoleApi
     {
         public long Id { get; set; }
         public string Name { get; set; }
+
+        public RoleApi(Roles role)
+        {
+            Name = role.Name;
+            Id = role.Id;
+        }
 
     }
 }
